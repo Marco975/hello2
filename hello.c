c7f4502e02f075bd89ab199e91d2eb3016ffc4da
Marco975 <54376560+Marco975@users.noreply.github.com>
2019-08-29 12:43:56 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Marco Martinez & Chong Jun Hong Vince
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Marco Martinez & Chong Jun Hong Vince\n");
+
+  return 0;
+}
